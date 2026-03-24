11edc94b6e2320b1b7d647aa290b99d47af9addd
Mohamed Taha <tahaaouini5@gmail.com>
2026-03-24 05:41:46 +0100

    fix: remove unused variables for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import { BrowserRouter } from "react-router-dom"
 import "./index.css"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
 import { TooltipProvider } from "@/components/ui/tooltip"
-import { PublicLayout } from "@/layouts/PublicLayout"
-import { DashboardLayout } from "@/layouts/DashboardLayout"
-import { Layout } from "./routes/enums"
+// import { PublicLayout } from "@/layouts/PublicLayout"
+// import { DashboardLayout } from "@/layouts/DashboardLayout"
+// import { Layout } from "./routes/enums"
 
-const layouts = {
-  [Layout.Public]: PublicLayout,
-  [Layout.Private]: DashboardLayout,
-}
+// const layouts = {
+//   [Layout.Public]: PublicLayout,
+//   [Layout.Private]: DashboardLayout,
+// }
 
 import App from "./App"
 
